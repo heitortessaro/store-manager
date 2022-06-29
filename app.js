7ffaf181c5d5e37bdb2b9728f61a147768fa1514
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-29 16:14:10 -0300

    feat: :sparkles: Create routes
    Create routes files for producs and sales. No request has been defined yet.

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const productsRouter = require('./routes/productsRouter');
+const salesRouter = require('./routes/salesRouter');
 
 // console.log(PORT);
 const app = express();
+app.use(express.json());
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.get('/test', (_req, res) => {
+  res.status(200).send('Test Ok');
+});
+
+app.use('/products', productsRouter);
+app.use('/sales', salesRouter);
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
