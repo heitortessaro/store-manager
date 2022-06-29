536f9599f5eef3124b110fae7b13ffb3596a3bff
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-29 18:11:48 -0300

    feat: :sparkles: Add getById to productModel
    Add function getById, which return false if the query return is invalid or return an array with an object

--- models/productModel.js
+++ models/productModel.js
@@ -1,10 +1,28 @@
 const connection = require('./connection');
 
 const getAll = async () => {
-  const [rows] = await connection.query('SELECT * FROM StoreManager.products');
-  return rows;
+  try {
+    const [rows] = await connection.query('SELECT * FROM StoreManager.products');
+    if (!rows) return false;
+    return rows; 
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getById = async (id) => {
+  try {
+    const [result] = await connection
+      .execute('SELECT * FROM StoreManager.products WHERE id = ?', [id]);
+    if (!result) return null;
+    console.log(result);
+    return result[0];
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = {
   getAll,
+  getById,
 };
