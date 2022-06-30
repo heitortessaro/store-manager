afe3aaaa8c129a706d95e2ea30911c8aabbccb42
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-30 19:30:38 -0300

    feat: :sparkles: Add route post /products
    This route enable user to add new products to the db passing the product name.

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -4,6 +4,7 @@ const router = require('express').Router();
 const productController = require('../controllers/productController');
 
 router.get('/', productController.getAll);
+router.post('/', productController.create);
 router.get('/:id', productController.getById);
 
 module.exports = router;
