b3fbab2c9b1f294bb338e955db1064fabacaa5f3
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-07-01 16:23:15 -0300

    feat: :sparkles: Create post sales route to create sales

--- routes/salesRouter.js
+++ routes/salesRouter.js
@@ -1,3 +1,7 @@
 const router = require('express').Router();
 
+const saleController = require('../controllers/saleController');
+
+router.post('/', saleController.createSale);
+
 module.exports = router;
