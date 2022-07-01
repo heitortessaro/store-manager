acc8a4d9ffe1a1e58021f6c22bf47d9f4488e961
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-07-01 18:50:47 -0300

    feat: :sparkles: Add getById to saleModel
    It expect the sale id. It returns objects with all products, quantities and the date related to that sale.

--- models/saleModel.js
+++ models/saleModel.js
@@ -2,11 +2,11 @@ const connection = require('./connection');
 const createException = require('../helpers/createException');
 
 const createSale = async () => {
-    const [result] = await connection.execute(
-      'INSERT INTO StoreManager.sales (date) VALUES (NOW());',
-    );
-    if (!result) createException(500, 'DB Error');
-    return { id: result.insertId };
+  const [result] = await connection.execute(
+    'INSERT INTO StoreManager.sales (date) VALUES (NOW());',
+  );
+  if (!result) createException(500, 'DB Error');
+  return { id: result.insertId };
 };
 
 const createSaleProduct = async ({ saleId, productId, quantity }) => {
@@ -33,8 +33,25 @@ const getAll = async () => {
   return rows;
 };
 
+const getById = async ({ id }) => {
+  const [rows] = await connection.execute(
+    `SELECT sp.sale_id AS saleId,
+      sp.product_id AS productId,
+      sp.quantity AS quantity,
+      s.date AS date
+      FROM StoreManager.sales_products AS sp
+      INNER JOIN StoreManager.sales AS s 
+      ON sp.sale_id = s.id
+      WHERE sp.sale_id = (?)
+      ORDER BY productId;`, [id],
+  );
+  if (!rows) createException(500, 'DB Error');
+  return rows;
+};
+
 module.exports = {
   createSale,
   createSaleProduct,
   getAll,
+  getById,
 };
