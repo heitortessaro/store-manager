6783a39f5919ec2cfe7aa5b94b7ad64eef4b18af
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-07-01 18:23:03 -0300

    feat: :sparkles: Create route get sales
    It returns all sales from the db. The response is composed by an array of objects, which has the saleId and its date,  the productId, and the  quantity.

--- routes/salesRouter.js
+++ routes/salesRouter.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const saleController = require('../controllers/saleController');
 
+router.get('/', saleController.getAll);
 router.post('/', saleController.createSale);
 
 module.exports = router;
