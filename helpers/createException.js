e43c93b05e3bc5c5bed7484b8f561cb3328037de
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-07-01 16:24:08 -0300

    chore: CreateException now throw an error

--- helpers/createException.js
+++ helpers/createException.js
@@ -1,5 +1,5 @@
 module.exports = (status, message) => {
   const error = new Error(message);
   error.status = status;
-  return error;
+  throw error;
 };
--- services/saleService.js
+++ services/saleService.js
@@ -14,7 +14,6 @@ const isSaleDataValid = (arr) => {
       ); 
     }
   });
-  return true;
 };
 
 const allProductsExistInDB = (arr, productsInDB) => {
