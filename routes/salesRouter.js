f359eed10ee139fa6b9403f6b3f258a759fa4151
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-07-03 16:23:16 -0300

    feat: :sparkles: Add delete route to sales
    it receives the sale id from the url. In case of sucess, the code 204 is returned

--- routes/salesRouter.js
+++ routes/salesRouter.js
@@ -5,5 +5,6 @@ const saleController = require('../controllers/saleController');
 router.get('/:id', saleController.getById);
 router.get('/', saleController.getAll);
 router.post('/', saleController.createSale);
+router.delete('/:id', saleController.del);
 
 module.exports = router;
