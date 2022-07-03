b1fc3b8a87638d79594aaaafd09a084c72640885
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-07-03 15:00:13 -0300

    feat: :sparkles: Create put rout to update products
    it receives the product id through the url and the new product name through the body

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -6,5 +6,6 @@ const productController = require('../controllers/productController');
 router.get('/', productController.getAll);
 router.post('/', productController.create);
 router.get('/:id', productController.getById);
+router.put('/:id', productController.update);
 
 module.exports = router;
