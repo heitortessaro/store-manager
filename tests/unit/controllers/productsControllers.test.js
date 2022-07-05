10b095584bb2778e82127a565860481730153c60
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-07-05 16:00:49 -0300

    test: :white_check_mark: Add tests to the del of productCOntroller

--- tests/unit/controllers/productsControllers.test.js
+++ tests/unit/controllers/productsControllers.test.js
@@ -212,5 +212,49 @@ describe("ProductController", () => {
         expect(res.status.calledWith(httpStatus.OK)).to.be.true;
       });
     });
-  })
+  });
+
+  describe("-> Calling the del from controller", () => {
+    describe("when an error is thrown", () => {
+      const req = {};
+      const res = {};
+      const message = "Product not found";
+      const error = new Error(message);
+      error.status = httpStatus.NOT_FOUND;
+      before(() => {
+        req.params = { id: 101 };
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.stub().returns(res);
+        sinon.stub(productService, "del").rejects(error);
+      });
+      after(() => {
+        productService.del.restore();
+      });
+      it("Expect a not found status", async () => {
+        const response = await productController.del(req, res);
+        expect(res.json.calledWith({ message })).to.be.equal(true);
+        expect(res.status.calledWith(httpStatus.NOT_FOUND)).to.be.equal(true);
+      });
+    });
+    describe("When the product is deleted", () => {
+      const req = {};
+      const res = {};
+      const id = 1;
+      const mockResponse = true;
+      before(() => {
+        req.params = { id };
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.stub().returns(res);
+        sinon.stub(productService, "del").resolves(mockResponse);
+      });
+      after(() => {
+        productService.del.restore();
+      });
+      it.only("Returns true, signaling that the object has been deleted", async () => {
+        const response = await productController.del(req, res);
+        expect(res.json.calledWith()).to.be.true;
+        expect(res.status.calledWith(httpStatus.OK_NOTHING_TO_RETURN)).to.be.true;
+      });
+    });
+  });
 });
